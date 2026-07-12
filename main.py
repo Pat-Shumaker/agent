a3764947b4adebb4a1d3731c4b6e057aa9e93d99
Pat-Shumaker <patrickshumaker12@gmail.com>
2026-07-12 21:22:06 +1000

    init client

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Load api key
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+
+if isinstance(api_key, str):
+    pass
+else:
+    raise RuntimeError("api key not retrieved")
+
+client = OpenAI(
+    base_url = "https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+messages=[
+    {
+        "role": "user",
+        "content": "one sentence test",
+    }
+]
+
+response = client.chat.completions.create(model="openrouter/free",
+                               messages=messages)
+
+print(response.choices[0].message.content)
+
 def main():
-    print("Hello from agent!")
+    pass
 
 
 if __name__ == "__main__":
